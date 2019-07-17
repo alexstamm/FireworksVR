f551922a9e52f267f235fdee27327658a97541be
Alex Stamm <stephen.alex.stamm@gmail.com>
2019-07-17 11:54:43 -0400

    adding steamvr action bindings

--- FireworksVR/Assets/Scripts/VRInput.cs
+++ FireworksVR/Assets/Scripts/VRInput.cs
@@ -1,14 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Valve.VR;
 
 public class VRInput : MonoBehaviour
 
 {
+    //player base ref
     public GameObject player;
+
+    //action ref
+    public SteamVR_Action_Boolean WorldPull;
+
+    //hand ref
+    public SteamVR_Input_Sources handType;
 
 
     void Awake()
+    {
+        isHandGrabbing = false;
+
+        WorldPull.AddOnStateDownListener(TriggerDown, handType);
+        WorldPull.AddOnStateUpListener(TriggerUp, handType);
+    }
+
+    public void TriggerUp(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
+    {
+        Debug.Log("Trigger Up");
+    }
+
+    public void TriggerDown(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
+    {
+        Debug.Log("Trigger is down");
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void SimplePull()
     {
 
     }
