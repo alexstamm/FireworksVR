36305a4983394e48f60eb0526b25de1081381815
Alex Stamm <stephen.alex.stamm@gmail.com>
2019-07-17 15:11:51 -0400

    making VFX graph adjustments to Fireworks.vfx

--- FireworksVR/Assets/Scripts/FireworkMGMT.cs
+++ FireworksVR/Assets/Scripts/FireworkMGMT.cs
@@ -4,14 +4,69 @@ using UnityEngine;
 
 public class FireworkMGMT : MonoBehaviour
 {
-    public float timeScale;
-
     public Vector2 gridSize;
 
+    public GameObject fireworkPrefab;
+
+    public Transform spawnLocations;
+    public List<Transform> spawnLocationList;
+
+    public List<GameObject> ActiveFireworks;
+
+    public int fireworkCountMax = 6;
+    private int fireworkCount;
+
+    private float delayTime;
+
+    public AnimationCurve curve;
+
+    public float timeTracker;
 
     void Awake()
     {
-        
+        fireworkCount = 0;
+        timeTracker = 0.0f;
+
+        foreach(Transform spawn in spawnLocations)
+        {
+            spawnLocationList.Add(spawn);
+        }      
     }
 
+    void Update()
+    {
+        if(fireworkCount < fireworkCountMax)
+        {
+            if(Time.frameCount % 20 == 0)
+            {
+                ShootFirework(fireworkCount);
+                fireworkCount++;
+            }
+ 
+        }
+
+
+        //motion
+
+
+
+        timeTracker += Time.deltaTime;
+        for(int i = 0; i < ActiveFireworks.Count; i++)
+        {
+            ActiveFireworks[i].transform.position = new Vector3(spawnLocationList[i].position.x, curve.Evaluate(timeTracker) * 20.0f, spawnLocationList[i].position.z);
+        }
+
+        if(timeTracker > 3.0f)
+        {
+            timeTracker = 0.0f;
+        }
+    }
+
+    void ShootFirework(int id)
+    {
+        float x = spawnLocationList[id].position.x;
+        float z = spawnLocationList[id].position.z;
+        GameObject go = Instantiate(fireworkPrefab, new Vector3(x, 0, z), Quaternion.identity);
+        ActiveFireworks.Add(go);
+    }
 }
