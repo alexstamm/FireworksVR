21404439681cdea59e2edc40f4b241dd1b91f4fa
Alex Stamm <stephen.alex.stamm@gmail.com>
2019-07-17 10:52:58 -0400

    adding base classes / scene

---
+++ FireworksVR/Assets/Scripts/FireworkMGMT.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireworkMGMT : MonoBehaviour
+{
+    public float timeScale;
+
+    public Vector2 gridSize;
+
+
+    void Awake()
+    {
+        
+    }
+
+}
---
+++ FireworksVR/Assets/Scripts/VRInput.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRInput : MonoBehaviour
+
+{
+    public GameObject player;
+
+
+    void Awake()
+    {
+
+    }
+
+}
